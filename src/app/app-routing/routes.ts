d202883cbfaa48663a4cb499eeb050ce4794588b
meenalsonwane <meenalsonwane@kizora.com>
2020-07-26 20:59:16 +0530

    spa part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -10,6 +10,7 @@ export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
   { path: 'about',  component: AboutComponent },
+  {path: 'dishdetail/:id' , component:DishdetailComponent},
   { path: 'contact',     component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
  
