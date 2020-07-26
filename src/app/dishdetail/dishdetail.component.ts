82d9af8b7537eac7e76a06a59442b6b6f1a1389e
meenalsonwane <meenalsonwane@kizora.com>
2020-07-26 22:25:33 +0530

    assignment 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -22,8 +22,8 @@ export class DishdetailComponent implements OnInit {
   }
 
   ngOnInit() {
-    let id =this.route.snapshot.params['id'];
-    this.dish =this.dishService.getDish(id);
+    const id =+this.route.snapshot.params['id'];
+    this.dish =this.dishservice.getDish(id);
   }
 
   goBack():void{
